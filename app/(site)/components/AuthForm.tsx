945848bf6eb76eb5b18f4f035a9b3a325ba66812
kwakseongjae <gkffhdnls13@gmail.com>
2023-09-29 14:32:02 +0900

    FEAT: Register Functionality - create account(register) 기능 추가

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,6 +1,7 @@
 // client component임을 알려준다.
 "use client";
 
+import axios from "axios";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { BsGithub, BsGoogle } from "react-icons/bs";
@@ -37,7 +38,7 @@ const AuthForm = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
     if (variant === "REGISTER") {
-      // Axios Register
+      axios.post("./api/register", data);
     }
 
     if (variant === "LOGIN") {
