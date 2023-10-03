21d368a733e6a5971f09e9bf6c7ec00abca7f01b
kwakseongjae <gkffhdnls13@gmail.com>
2023-10-03 23:15:20 +0900

    FEAT: User screen 추가 - app>actions>getUsers.tsx 파일 추가 - app>users>layout.tsx 파일에 getUsers를 import하여여 users 변수 전달 - app>users>components 폴더에 users 변수를 사용하는 UserList.tsx파일 추가 및 각각의 user 데이터를 매핑하여 UserBox에 전달

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -87,19 +87,19 @@ const AuthForm = () => {
   return (
     <div
       className="
-  mt-8
-  sm:mx-auto
-  sm:w-full
-  sm:max-w-md"
+        mt-8
+        sm:mx-auto
+        sm:w-full
+        sm:max-w-md"
     >
       <div
         className="
-        bg-white
-        px-4
-        py-8
-        shadow
-        sm:rounded-lg
-        sm:px-10"
+          bg-white
+          px-4
+          py-8
+          shadow
+          sm:rounded-lg
+          sm:px-10"
       >
         <form
           className="
